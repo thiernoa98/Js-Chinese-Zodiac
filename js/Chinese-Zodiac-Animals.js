65b6966a7f153c78fb98bb9f483d9309fa1e717f
Thierno Barry <117252418+thiernoa98@users.noreply.github.com>
2023-03-07 18:32:36 -0500

    Update Chinese-Zodiac-Animals.js

--- js/Chinese-Zodiac-Animals.js
+++ js/Chinese-Zodiac-Animals.js
@@ -12,7 +12,7 @@ for(let i = 0; i < animals.length; i++) {
 
     pic.src = `images/animals/${animal.eng}.jpg`;
 
-    // 18. Apply the **.animal-pic** class to the image:
+    //Apply the animal-pic class to the image:
     pic.className = 'animal-pic';
 
     divvy.appendChild(pic);
@@ -22,7 +22,7 @@ for(let i = 0; i < animals.length; i++) {
 
     inputBox.type = 'search';
 
-    // a placeholder** to prompt the user to enter the animal name:
+    // a placeholder to prompt the user to enter the animal name:
     inputBox.placeholder = 'name';
 
     inputBox.eng = animal.eng;
@@ -46,9 +46,8 @@ for(let i = 0; i < animals.length; i++) {
     divvy.appendChild(pTag);
 
     //chinese zodiac years: 12 in between each year
-    let yearSeries = (animal.year - 157) + ' '; //12 * 12 num of years 144
-    //12 * 12 == 144
-    //here y-= == 144 - 12 ...
+    let yearSeries = (animal.year - 157) + ' ';
+    
     for (let y = 144; y >= -12; y-=12) {
        yearSeries+= (animal.year - y) + ' ';
     }
@@ -64,28 +63,22 @@ for(let i = 0; i < animals.length; i++) {
 
 
 
-    //ceate a span for the english name on top
+    //ceate a span for the chinese name on on the bottom
     let chinese = document.createElement('span');
     chinese.innerHTML = animal.pin;
     chinese.className = 'pinyin';
     chinese.id = 'pin' + i;
     divvy.appendChild(chinese);
 
-    // Lesson Part II (B.)
-    // 5B. - 9B. make an image and set its source to sound-icon.png:
-    // apply class and have icon call playSound func when clicked
     const soundIcon = new Image();
     soundIcon.src = 'images/sound-icon.png'; //image
     soundIcon.className = 'sound-icon'; //fro, the css
     soundIcon.eng = animal.eng; //making a object
     soundIcon.addEventListener('click', playSound);
     divvy.appendChild(soundIcon);
-} // end loop
+} 
 
-// 10B. in the global scope, instantiate an instance 
-// of the Audio object. It's the same synta as the Image object, 
-// with the new keyword:
-//call the sound
+//call the animal sound
 const sound = new Audio();
 function playSound() {
     //stop the sound that is playing
@@ -94,20 +87,11 @@ function playSound() {
     sound.play();
 }
 
-// 20. Reload the page. We should have 12 divs, each with the cow.
-
-// We need to make the **checkSpelling** function before reloading the page in the browser. If the function referred to in the listener does not exist at that point, we'll get an error.
-
-// this keyword
-
-// Inside a function, the **this** keyword refers to the object that called the function. In the **checkSpelling** function, **this** is the input box:
-
-// 32. Define the **checkSpelling** function and start by getting the **value** of the input element--whatever the user typed into the box:
 function checkSpelling() {
     let input = this.value;
-    // 33. Compare the user input to the English, pinyin and alternate (also) spelling. The user input needs to match *one* of the three correct spellings:
+    
+     // check If the user input is correct, turn the input box green; else turn the box red:
     if(input == this.eng || input == this.chi || input == this.also) {
-        // 34. If the user input is correct, turn the input box green; else turn the box red:
         this.style.backgroundColor = '#0B0';
 
         //show english
@@ -124,12 +108,3 @@ function checkSpelling() {
     }
     this.style.color = '#fff';
 }
- 
-// 35. Reload the page. Each box should have an animal pic and an input box.
-
-// 36. Enter a spelling in a box and hit Enter. 
-// - if the spelling is correct, the input box turns green. 
-// - else, the spelling is incorret, so the box turns red.
-
-// END: Lesson 06.02
-// NEXT: Lesson 06.03
