095438637ba38293e19c6bc8ddf228ee74e5ca97
Thierno Barry <117252418+thiernoa98@users.noreply.github.com>
2023-03-10 20:02:36 -0500

    Add files via upload

---
+++ js/chinese-zodiac-animals-data.js
@@ -0,0 +1,14 @@
+let animals = [ 
+    {eng: "chicken", also: 'rooster', chi: "ji", pin: "ji&#772;", year: 2029, tone: 1},
+    {eng: "cow", also: 'ox', chi: "niu", pin: "niu&#769;",  year: 2021, tone: 2},
+    {eng: "dog", also: null, chi: "gou", pin: "gou&#780;",  year: 2030, tone:3},
+    {eng: "dragon", also: null, chi: "long", pin: "lo&#769;ng", year: 2024, tone: 2},
+    {eng: "goat", also: 'sheep', chi: "yang", pin: "ya&#769;ng", year: 2027, tone: 2},
+    {eng: "horse", also: null, chi: "ma", pin: "ma&#780;", year: 2026, tone: 3},
+    {eng: "monkey", chi: "hou", also: 'houzi', pin: "ho&#769;uzi", year: 2028, tone: 2},
+    {eng: "pig", also: null, chi: "zhu", pin: "zhu&#772;", year: 2031, tone: 1},
+    {eng: "rabbit", chi: "tu", also: 'tuzi', pin: "tu&#768;zi", year: 2023, tone: 4},
+    {eng: "rat", chi: "shu", also: 'laoshu', pin: "la&#780;oshu&#780;", year: 2020, tone: 3},
+    {eng: "snake", also: null, chi: "she", pin: "she&#769;", year: 2025, tone: 2},
+    {eng: "tiger", chi: "hu", also: 'laohu', pin: "la&#780;ohu&#780;", year: 2022, tone: 3} 
+];
